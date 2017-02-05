d3df2980803c0dbf7a19c6502c157dad7f461b44
skywet <skywet@yeah.net>
2017-02-05 11:35:16 +0800

    update

---
+++ ID.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+import re
+import time
+
+rt = time.localtime()
+rm = rt[1]
+f = open(r'ID.txt','r',encoding='utf-8')
+for line in f:
+    lst = re.split(r'[ \t]+',line)
+    bd = lst[3]
+    bm = re.split('/',bd)
+    if int(rm)==int(bm[1]):
+        print('ID:{ID}     姓名：{name}   生日：{birthday}'.format(ID=lst[0],name=lst[1],birthday=bd))
