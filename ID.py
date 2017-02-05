783f8c28c7fe6809beac59e4e40506ae72171028
skywet <skywet@yeah.net>
2017-02-05 15:46:31 +0800

    next

--- ID.py
+++ ID.py
@@ -10,4 +10,5 @@ for line in f:
     bd = lst[3]
     bm = re.split('/',bd)
     if int(rm)==int(bm[1]):
-        print('ID:{ID}     姓名：{name}   生日：{birthday}'.format(ID=lst[0],name=lst[1],birthday=bd))
+        dat = int(bm[2])-rt[2]
+        print('ID:{ID}   姓名：{name}   生日：{birthday}  剩余时间：{date}'.format(ID=lst[0],name=lst[1],birthday=bd,date=dat))
