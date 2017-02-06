dbfc76acd43891d311ccc83ea786cddeca935764
skywet <skywet@yeah.net>
2017-02-06 19:35:24 +0800

    a

--- tk.py
+++ tk.py
@@ -10,7 +10,7 @@ driver.find_element_by_name('uname').send_keys('skywet')
 driver.find_element_by_name('upwd').send_keys('1101221wtyjack')
 driver.find_element_by_class_name('sbmitys').click()
 sleep(3)
-driver.get(r'http://www.swtk.cn/team/team.php?tid=353')
+driver.get(r'http://www.swtk.cn/team/member.php?tid=353')
 driver.find_element_by_link_text('风中笑霸').click()
 sleep(2)
 qzx = driver.page_source
